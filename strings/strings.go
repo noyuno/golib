be9dd0cd00467aafe52071fc6e037cb3819fa50e
noyuno <noyuno@users.noreply.github.com>
2017-10-22 17:13:33 +0900

    Update

--- strings/strings.go
+++ strings/strings.go
@@ -1,8 +1,28 @@
 package strings
 
-func Index(s []rune, str []rune) int {
-	var i int = 0
-	var p int = 0
+// Index : 最初に現れる文字の位置を返す
+// s: 対象文字列
+// str: 検索文字列
+// start: sの先頭を0としたstrの開始位置
+// return: sの先頭を0としたstrの開始位置.
+// len(s) < len(str)であれば-1を返す．
+// strが空文字であればmin(len(s), start)を返す．
+// strを見つけることができなければ，-1を返す．
+func Index(s []rune, str []rune, start int) int {
+	i := 0
+	if start > 0 {
+		i = start
+	}
+	p := 0
+	if len(s) < len(str) {
+		return -1
+	}
+	if len(str) == 0 {
+		if start < len(s) {
+			return start
+		}
+		return len(s)
+	}
 	for ; i < len(s) && p < len(str); i++ {
 		if s[i] == str[p] {
 			p += 1
--- strings/strings_test.go
+++ strings/strings_test.go
@@ -2,16 +2,18 @@ package strings
 
 import "testing"
 
-func tindex(t *testing.T, s string, str string, expected int) {
-	actual := Index([]rune(s), []rune(str))
+func tindex(t *testing.T, s string, str string, start int, expected int) {
+	actual := Index([]rune(s), []rune(str), start)
 	if actual != expected {
-		t.Errorf("want %v, but %v", expected, actual)
+		t.Errorf("Index(%s, %s, %d) want %v, but %v", s, str, start, expected, actual)
 	}
 }
 
 func TestIndex(t *testing.T) {
-	tindex(t, "abcde", "d", 3)
-	tindex(t, "abcde", "o", -1)
-	tindex(t, "あえいうえお", "えお", 4)
-	tindex(t, "カキクケキクケキクケコ", "キクケコ", 7)
+	tindex(t, "abcde", "d", 0, 3)
+	tindex(t, "abcde", "o", 2, -1)
+	tindex(t, "あえいうえお", "えお", 0, 4)
+	tindex(t, "あい", "あいうえお", 0, -1)
+	tindex(t, "カキクケキクケキクケコ", "キクケ", 3, 4)
+	tindex(t, "カキクケキクケキクケコ", "", 3, 3)
 }
